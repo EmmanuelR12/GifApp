fa6789a8778448a0948d30795b19c6c38be44e92
EmmanuelR12 <erangel@tecnolika.com>
2022-05-16 12:32:57 -0500

    images temrinado

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+  constructor (private gifService:GifsService){
+
+  }
+  get historial(){
+    return this.gifService.historial
+  }
+
+  busqueda(image:string){
+    this.gifService.buscarGifs(image);
+  }
+}
